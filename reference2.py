efe84dd442b3d857920b34074f6350d0ee509efa
juto-shogan <somtombonu53@gmail.com>
2025-06-02 23:21:16 +0100

    Practiced column making

--- reference2.py
+++ reference2.py
@@ -5,6 +5,7 @@ import numpy as np
 # Create two columns
 col1, col2 = st.columns(2)
 
+col3, col4 = st.columns(2)
 # First plot in the first column
 with col1:
     st.header("Plot 1")
@@ -14,6 +15,19 @@ with col1:
 
 # Second plot in the second column
 with col2:
+    st.header("Plot 2")
+    fig2, ax2 = plt.subplots()
+    ax2.plot(np.random.randn(10), color='orange')
+    st.pyplot(fig2)
+    
+with col3:
+    st.header("Plot 1")
+    fig1, ax1 = plt.subplots()
+    ax1.plot(np.random.randn(10))
+    st.pyplot(fig1)
+
+# Second plot in the second column
+with col4:
     st.header("Plot 2")
     fig2, ax2 = plt.subplots()
     ax2.plot(np.random.randn(10), color='orange')
